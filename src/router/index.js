c06715a1c371bb79a70021a8c36cb3837e0f686e
Web前端-吴溥毅 <wupuyi@foxmail.com>
2018-01-16 17:35:11 +0800

    添加购买页面子路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import Vuex from 'vuex'
 // import HelloWorld from '@/components/HelloWorld'
 import IndexPage from '@/pages/Index'
+import DetailPage from '@/pages/detail'
+import DetailAnaPage from '@/pages/detail/analysis'
+import DetailCouPage from '@/pages/detail/count'
+import DetailForPage from '@/pages/detail/forecast'
+import DetailPubPage from '@/pages/detail/publish'
 
 Vue.use(Router)
 Vue.use(Vuex)
@@ -15,6 +20,30 @@ export default new Router({
       path: '/',
       name: 'IndexPage',
       component: IndexPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage,
+      // 配置自路由
+      children: [
+        {
+          path: 'analysis',
+          component: DetailAnaPage
+        },
+        {
+          path: 'count',
+          component: DetailCouPage
+        },
+        {
+          // 自路由path前不加 /
+          path: 'forecast',
+          component: DetailForPage
+        },
+        {
+          path: 'publish',
+          component: DetailPubPage
+        }
+      ]
     }
   ]
 })
